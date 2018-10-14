89cd8e00abda67c24b0dc9a7fdee3ffa8440d6ca
hitmontop(notebook) <pinkjery@naver.com>
2018-10-14 22:42:18 +0900

    유닛의 인공지능, 공격 구현
    유닛의 시야, 사거리, 공격을 구현했다.
    시야에 들어온 적 중에서 가장 거리가 가까운 적을 '락온' 하고,  사거리 안에 들어올 때 까지 추격한 후, 둘중 하나가 죽을때까지 쫒아가서 싸우는 인공지능 이다.

--- ageofwar.py
+++ ageofwar.py
@@ -17,37 +17,91 @@ class unit:
         self.is_foe
         self.target
         self.image
+        self.is_lock_on = False
 
     def update(self):
-        if self.is_foe:
-            if unit.search_for_enemy():
-                if self.x - self.range > self.target.x or self.target.x > self.x + self.range:
-                    self.x = self.x - self.speed
+        if 0 > self.x or self.x > 800 or 0 > self.y or self.x > 600:
+            self.hp=0
+        unit.check_my_hp()
+
+        if self.is_lock_on:
+            if self.x < self.target.x:
+                if self.x - self.range < self.target.x:
+                    self.x = self.x + self.speed
+                else:
+                    unit.attack(self.dmg)
             else:
-                self.x = self.x - self.speed
+                if self.x - self.range > self.target.x:
+                    self.x = self.x - self.speed
+                else:
+                    unit.attack(self.dmg)
+
         else:
             if unit.search_for_enemy():
-                if self.x - self.range > self.target.x or self.target.x > self.x + self.range:
-                    self.x = self.x + self.speed
+                if self.x < self.target.x:
+                    if self.x - self.range < self.target.x:
+                        self.x = self.x + self.speed
+                    else:
+                        unit.attack(self.dmg)
+                else:
+                    if self.x - self.range > self.target.x:
+                        self.x = self.x - self.speed
+                    else:
+                        unit.attack(self.dmg)
             else:
-                self.x = self.x + self.speed
+
+                if self.is_foe:
+                    self.x = self.x - self.speed
+                    self.y = self.y + 0.5
+                else:
+                    self.x = self.x + self.speed
+                    self.y = self.y + 0.5
+
+
 
 
     def search_for_enemy(self):
+        min= 10000
+
         if self.is_foe:
             for i in player_units:
                 if self.x - self.sight < i.x < self.x + self.sight:
-                    self.target = i
-                    return True
+                    if min > i.x:
+                        min = i.x
+                        self.target = i
+                        self.is_lock_on = True
+
+            if self.is_lock_on:
+                return True
+
         else:
             for i in computer_units:
                 if self.x - self.sight < i.x < self.x + self.sight:
-                    self.target = i
-                    return True
+                    if min > i.x:
+                        min = i.x
+                        self.target = i
+                        self.is_lock_on = True
+
+            if self.is_lock_on:
+                return True
 
 
-    def attack(self):
-        pass
+    def attack(self, dmg):
+        self.target.hp = self.target.hp - dmg
+        if self.target.hp <= 0:
+            self.is_lock_on = False
+
+
+    def check_my_hp(self):
+        if self.hp <= 0:
+            unit.death()
+
+    def death(self):
+        if self.is_foe:
+            computer_units.remove(self)
+        else:
+            player_units.remove(self)
+
 
     def draw(self):
         self.image.draw(self.x, self.y)
@@ -56,13 +110,14 @@ class unit:
 
 class ant(unit):
     def __init__(self, x, y, is_foe):
-        self.hp = 100
+        self.hp = 1500
         self.dmg = 10
-        self.sight = 6000
-        self.range = 10
+        self.sight = 100
+        self.range = 2
         self.speed = 1
         self.x, self.y = x, y
         self.is_foe = is_foe
+        self.is_lock_on = False
 
         if self.is_foe:
             self.image = load_image('ball21X21.png')
@@ -100,21 +155,22 @@ def handle_events():
 player_units = []
 computer_units = []
 
-player_units.append(ant(200, 200, False))
-computer_units.append(ant(600, 400, True))
-player_units.append(ant(100, 300, False))
-
 
 # game main loop
+
+cnt=0
 running = True
 while running:
     handle_events()
+
+    if cnt % 50 == 0:
+        computer_units.append(ant(random.randint(10,600), random.randint(200,300), True))
+        player_units.append(ant(random.randint(10,600), random.randint(200,300), False))
 
     for unit in player_units:
         unit.update()
     for unit in computer_units:
         unit.update()
-
     clear_canvas()
 
     for unit in player_units:
@@ -125,7 +181,7 @@ while running:
     update_canvas()
 
     delay(0.01)
-
+    cnt = cnt+1
 
 
 
