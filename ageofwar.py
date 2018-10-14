f84a17ea51b152a316e6f7af249d784be3237fb7
hitmontop(notebook) <pinkjery@naver.com>
2018-10-15 01:00:50 +0900

    근접유닛, 원거리유닛, 투사체
    공은 원거리 유닛, 소년은 근접유닛으로 설정했다.
    투사체 클래스를 정의했다.
    이제 공은 적에게 투사체를 쏴서 공격할 수 있다.

--- ageofwar.py
+++ ageofwar.py
@@ -5,7 +5,7 @@ open_canvas()
 
 
 # game object class
-class unit:
+class Unit:
     def __init__(self):
         self.hp
         self.dmg
@@ -18,50 +18,56 @@ class unit:
         self.target
         self.image
         self.is_lock_on = False
+        self.is_melee
 
     def update(self):
         if 0 > self.x or self.x > 800 or 0 > self.y or self.y > 600:
-            self.hp=0
+            Unit.death()
 
-        unit.check_my_hp()
+        Unit.check_my_hp()
+
+        if self.is_lock_on:
+            if self.target.hp <= 0:
+                self.is_lock_on = False
 
         if self.is_lock_on:
             if self.x < self.target.x:
                 if self.x + self.range < self.target.x:
                     self.x = self.x + self.speed
                 else:
-                    unit.attack(self.dmg)
+                    Unit.attack(self.dmg)
             else:
                 if self.x - self.range > self.target.x:
                     self.x = self.x - self.speed
                 else:
-                    unit.attack(self.dmg)
+                    Unit.attack(self.dmg)
 
         else:
-            if unit.search_for_enemy():
+            if Unit.search_for_enemy():
                 if self.x < self.target.x:
                     if self.x - self.range < self.target.x:
                         self.x = self.x + self.speed
                     else:
-                        unit.attack(self.dmg)
+                        Unit.attack(self.dmg)
                 else:
                     if self.x - self.range > self.target.x:
                         self.x = self.x - self.speed
                     else:
-                        unit.attack(self.dmg)
-            else:
+                        Unit.attack(self.dmg)
 
+            else:
                 if self.is_foe:
                     self.x = self.x - self.speed
-                    self.y = self.y + 0.5
+
                 else:
                     self.x = self.x + self.speed
-                    self.y = self.y + 0.5
+
 
 
 
 
     def search_for_enemy(self):
+
         min= 10000
 
         if self.is_foe:
@@ -88,14 +94,16 @@ class unit:
 
 
     def attack(self, dmg):
-        self.target.hp = self.target.hp - dmg
-        if self.target.hp <= 0:
-            self.is_lock_on = False
+        if self.is_melee:
+            self.target.hp = self.target.hp - dmg
+        else:
+            Spitter_ant_projectile(self.x, self.y, self.target)
+
 
 
     def check_my_hp(self):
         if self.hp <= 0:
-            unit.death()
+            Unit.death()
 
     def death(self):
         if self.is_foe:
@@ -103,43 +111,88 @@ class unit:
         else:
             player_units.remove(self)
 
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
+
+class Ant(Unit):
+    def __init__(self, x, y, is_foe):
+        self.hp = 5000
+        self.dmg = 100
+        self.sight = 100
+        self.range = 2
+        self.speed = 1.5
+        self.x, self.y = x, y
+        self.is_foe = is_foe
+        self.is_lock_on = False
+        self.is_melee = True
+
+        self.image = load_image('character.png')
+
+
+class Spitter_ant(Unit):
+    def __init__(self, x, y, is_foe):
+        self.hp = 1500
+        self.dmg = 2
+        self.sight = 300
+        self.range = 100
+        self.speed = 1.0
+        self.x, self.y = x, y
+        self.is_foe = is_foe
+        self.is_lock_on = False
+        self.is_melee = False
+
+        self.image = load_image('ball41X41.png')
+
+
+
+class Projectile:
+    def __init__(self):
+        self.speed
+        self.x, self.y
+        self.image
+        self.target
+        self.dmg
+        self.destination_x, self.destination_y
+        self.i = 0
+
+    def update(self):
+        self.destination_x, self.destination_y = self.target.x, self.target.y
+
+        if self.i < 100:
+            self.i = self.i + 2
+            t = self.i / 100
+            self.x = (1 - t) * self.x + t * self.destination_x
+            self.y = (1 - t) * self.y + t * self.destination_y
+
+        if self.i == 100:
+            if self.target.hp > 0:
+                self.target.hp = self.target.hp - self.dmg
+            projectiles.remove(self)
 
     def draw(self):
         self.image.draw(self.x, self.y)
 
 
 
-class ant(unit):
-    def __init__(self, x, y, is_foe):
-        self.hp = 1500
-        self.dmg = 10
-        self.sight = 100
-        self.range = 50
+
+class Spitter_ant_projectile(Projectile):
+    def __init__(self, x, y, target):
+
         self.speed = 3.0
+        self.i = 0
         self.x, self.y = x, y
-        self.is_foe = is_foe
-        self.is_lock_on = False
+        self.dmg = 1000
+        self.target = target
+        self.destination_x, self.destination_y = self.target.x, self.target.y
 
-        if self.is_foe:
-            self.image = load_image('ball21X21.png')
-        else:
-            self.image = load_image('ball41X41.png')
+        self.image = load_image('flame.png')
+
+        projectiles.append(self)
 
 
-'''
-class Boy:
-    def __init__(self):
-        self.x, self.y = random.randint(100, 700), 90
-        self.frame = random.randint(0, 7)
-        self.image = load_image('run_animation.png')
 
-    def update(self):
-        self.frame = (self.frame + 1) % 8
-        self.x += 5
-
-    def draw(self):
-        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
-'''
 
 def handle_events():
     global running
@@ -151,11 +204,10 @@ def handle_events():
             running = False
 
 
-
 # initialization code
 player_units = []
 computer_units = []
-
+projectiles = []
 
 # game main loop
 
@@ -164,20 +216,25 @@ running = True
 while running:
     handle_events()
 
-    if cnt % 50 == 0:
-        computer_units.append(ant(random.randint(10,600), random.randint(200,300), True))
-        player_units.append(ant(random.randint(10,600), random.randint(200,300), False))
+    if cnt % 100 == 0:
+        computer_units.append(Spitter_ant(random.randint(10, 600), random.randint(200, 300), True))
+        player_units.append(Ant(random.randint(10, 600), random.randint(200, 300), False))
 
-    for unit in player_units:
-        unit.update()
-    for unit in computer_units:
-        unit.update()
+    for Unit in player_units:
+        Unit.update()
+    for Unit in computer_units:
+        Unit.update()
+    for obj in projectiles:
+        obj.update()
+
     clear_canvas()
 
-    for unit in player_units:
-        unit.draw()
-    for unit in computer_units:
-        unit.draw()
+    for Unit in player_units:
+        Unit.draw()
+    for Unit in computer_units:
+        Unit.draw()
+    for obj in projectiles:
+        obj.draw()
 
     update_canvas()
 
